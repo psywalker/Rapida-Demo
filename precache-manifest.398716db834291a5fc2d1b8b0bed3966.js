7ac80d49cfa53f0e4708481471c757b613792d69
psywalker <psywalker09@gmail.com>
2019-04-11 22:36:26 +0300

    Updates

---
+++ precache-manifest.398716db834291a5fc2d1b8b0bed3966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4eee15a40bc9c75868a5",
+    "url": "/Rapida-Demo/static/css/main.e8cf6be5.chunk.css"
+  },
+  {
+    "revision": "4eee15a40bc9c75868a5",
+    "url": "/Rapida-Demo/static/js/main.bcbf9025.chunk.js"
+  },
+  {
+    "revision": "7e9ff0ed1a7f93ee490a",
+    "url": "/Rapida-Demo/static/js/runtime~main.6567ce07.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Rapida-Demo/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Rapida-Demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3aec6c9f56b0f25580790c2290920b7c",
+    "url": "/Rapida-Demo/index.html"
+  }
+];
